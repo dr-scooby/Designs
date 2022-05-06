843c57c1ddd2718a65ee60a252c1ecba25fa1c2b
Jahangir Ismail <81840866+dr-scooby@users.noreply.github.com>
2022-05-06 14:11:02 -0700

    Create CommercialPlan.java

---
+++ CommercialPlan.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package plans;
+
+/**
+ * @author jismailx
+ *
+ */
+public class CommercialPlan extends Plan{
+
+	@Override
+	public void getRate() {
+		// TODO Auto-generated method stub
+		rate = 7.50;
+	}
+
+	
+}
