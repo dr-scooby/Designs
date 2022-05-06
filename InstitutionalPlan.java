da08a9c7eb7b83d6d688734df0b333fed7198261
Jahangir Ismail <81840866+dr-scooby@users.noreply.github.com>
2022-05-06 14:11:40 -0700

    Create InstitutionalPlan.java

---
+++ InstitutionalPlan.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package plans;
+
+/**
+ * @author jismailx
+ *
+ */
+public class InstitutionalPlan extends Plan{
+
+	@Override
+	public void getRate() {
+		// TODO Auto-generated method stub
+		rate = 5.50;
+	}
+
+}
